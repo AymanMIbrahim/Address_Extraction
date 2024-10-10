aef9218ab6cfd7e41efea09e483876e136072661
Ayman M. Ibrahim <ayman.m.ibrahim.1994@gmail.com>
2024-10-10 14:57:18 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,request
+from src.logic import ExtractAddress
+
+
+app = Flask(__name__)
+
+@app.route('/get_extracted_address', methods=['POST'])
+def get_extracted_address():
+    address1 = request.json['address1']
+    address2 = request.json['address2']
+    print(address1)
+    print(address2)
+    pipline = ExtractAddress(address1,address2)
+    Information = pipline.get_address()
+
+
+    return Information
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", 7000)
+
+
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages
+from setuptools import setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(path:str) -> List[str]:
+    '''
+    This function will return list of requirements
+    '''
+
+
+
+    with open(path) as f:
+        req = [r.replace("\n","") for r in f.readlines()]
+        if HYPEN_E_DOT in req:
+            req.remove(HYPEN_E_DOT)
+    return req
+
+setup(
+    name='Validation_Employees',
+    version='0.0.1',
+    author='Ayman M. Ibrahim',
+    author_email='ayman.ibrahim@theaddressholding.com',
+    packages=find_packages(),
+    install_requires=get_requirements('./requirments.txt')
+)
