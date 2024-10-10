2e8453fe607a7c7c7aeab9f6a140557d4c3b9484
AymanMIbrahim <ayman.m.ibrahim.1994@gmail.com>
2024-10-10 18:27:36 +0300

    Cores Included

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask,request
+from flask_cors import CORS
 from src.logic import ExtractAddress
 
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/get_extracted_address', methods=['POST'])
 def get_extracted_address():
