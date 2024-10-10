3542a55ed3882669d25d015cc1273637d17c7fc8
AymanMIbrahim <ayman.m.ibrahim.1994@gmail.com>
2024-10-10 20:33:47 +0300

    SSL

--- app.py
+++ app.py
@@ -35,6 +35,6 @@ def get_extracted_address():
         return Information
 
 if __name__ == '__main__':
-    app.run("0.0.0.0", 7000)
+    app.run(host='0.0.0.0', port=7000, ssl_context=('cert.pem', 'key.pem'))
 
 
