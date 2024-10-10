c5c428749d2aa1a32c84e245c38b6cccda059d63
AymanMIbrahim <ayman.m.ibrahim.1994@gmail.com>
2024-10-10 15:09:35 +0300

    Nesrein and Omar Code

--- src/logic.py
+++ src/logic.py
@@ -7,7 +7,17 @@ class ExtractAddress:
         self.address1 = address1
         self.address2 = address2
 
-    def get_address(self):
+
+    def get_address_nesrien_omar(self):
+        Information = {"bulid_no": "", "street": "", "village": "", "district": "", "gov": ""}
+
+        gov,district = self.address2.split("-")[0],self.address2.split("-")[1]
+        PureAddress1 = self.address1.replace(self.address2, "")
+
+
+        return Information
+
+    def get_address_ayman(self):
         Information = {"bulid_no": "", "street": "", "village": "", "district": "", "gov": ""}
 
         gov,district = self.address2.split("-")[0],self.address2.split("-")[1]
