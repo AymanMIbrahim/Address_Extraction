4f8566cfbf8fb82da8d54a33f5d9d57492b74d49
AymanMIbrahim <ayman.m.ibrahim.1994@gmail.com>
2024-10-14 15:37:58 +0300

    Add Security

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask,request,Response
+from flask import Flask,request,Response,abort
 from flask_cors import CORS
 from src.logic import ExtractAddress
 
 
 app = Flask(__name__)
 CORS(app)
+
+API_KEY = "D3$xYp8!nQ4&fJ6v"
+def verify_api_key():
+    api_key = request.headers.get('Authorization')
+    if api_key != API_KEY:
+        abort(401, description="Unauthorized: Invalid API key")
 
 @app.after_request
 def apply_csp(response: Response):
@@ -13,26 +19,28 @@ def apply_csp(response: Response):
 
 @app.route('/get_extracted_address', methods=['POST'])
 def get_extracted_address():
-    try:
-        address1 = request.json['address1']
-        address2 = request.json['address2']
-        pipline = ExtractAddress(address1,address2)
-        Information = pipline.get_address_information()
-        Information["status"] = "success"
+    #try:
+    verify_api_key()
+
+    address1 = request.json['address1']
+    address2 = request.json['address2']
+    pipline = ExtractAddress(address1,address2)
+    Information = pipline.get_address_information()
+    Information["status"] = "success"
 
 
-        return Information
+    return Information
 
-    except:
-        Information = {
-            "status": "falied",
-            "bulid_no": "",
-            "street": "",
-            "village": "",  # Not handled as per instructions
-            "district": "",
-            "gov": ""
-        }
-        return Information
+    # except:
+    #     Information = {
+    #         "status": "falied",
+    #         "bulid_no": "",
+    #         "street": "",
+    #         "village": "",  # Not handled as per instructions
+    #         "district": "",
+    #         "gov": ""
+    #     }
+    #     return Information
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8069)
